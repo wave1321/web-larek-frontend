29aec4b1e2d22e1a3e4abf9ec07de2184f6145c5
Sergey Bovin <snowbovin@gmail.com>
2025-04-27 21:48:14 +0300

    Review Day

---
+++ src/components/base/Api.ts
@@ -0,0 +1,47 @@
+export type ApiListResponse<Type> = {
+    total: number,
+    items: Type[]
+};
+
+export type ApiOrderResponse = {
+    id: string;
+    total: number;
+}
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+export class Api {
+    readonly baseUrl: string;
+    protected options: RequestInit;
+
+    constructor(baseUrl: string, options: RequestInit = {}) {
+        this.baseUrl = baseUrl;
+        this.options = {
+            headers: {
+                'Content-Type': 'application/json',
+                ...(options.headers as object ?? {})
+            }
+        };
+    }
+
+    protected handleResponse<T>(response: Response): Promise<T> {
+        if (response.ok) return response.json();
+        else return response.json()
+            .then(data => Promise.reject(data.error ?? response.statusText));
+    }
+
+    get<T>(uri: string) {
+        return fetch(this.baseUrl + uri, {
+            ...this.options,
+            method: 'GET'
+        }).then(this.handleResponse<T>);
+    }
+
+    post<T>(uri: string, data: object, method: ApiPostMethods = 'POST') {
+        return fetch(this.baseUrl + uri, {
+            ...this.options,
+            method,
+            body: JSON.stringify(data)
+        }).then(this.handleResponse<T>);
+    }
+}
